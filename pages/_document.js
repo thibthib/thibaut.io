b80ca3f5fc0975f71eb58480a1d71f245472c2ab
Thibaut Dutartre <t.dutartre@gmail.com>
2018-12-05 10:22:43 +0100

    Restore inline styles

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
-import Document, { Main, NextScript } from 'next/document';
+import Document, { Main, NextScript, Head } from 'next/document';
 
 export default class MyDocument extends Document {
     render() {
         return (
             <html lang="en">
+                <Head>{this.props.styles}</Head>
                 <body>
                     <Main />
                     <NextScript />
