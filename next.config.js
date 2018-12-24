30f6368f9f1846833b30ae929c08e6f0ffebb591
Thibaut Dutartre <t.dutartre@gmail.com>
2018-12-24 16:02:27 +0100

    Export duple page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     exportPathMap: function() {
         return {
-            '/': { page: '/' }
+            '/': { page: '/' },
+            '/duple': { page: '/duple' }
         };
     }
 };
