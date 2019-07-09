d715fd4a6cc3a7c0670b9b0800c44768435eef17
Thibaut Dutartre <t.dutartre@gmail.com>
2019-07-09 10:35:29 -0400

    Test

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ const withMDX = require('@next/mdx')();
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
+  target: 'serverless',
   exportPathMap: function() {
     return {
       '/': { page: '/' },
       '/duple': { page: '/duple' },
+      '/refs': { page: '/refs' },
     };
   },
 });
---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import { MDXProvider } from '@mdx-js/react';
+import { css } from '@emotion/core';
+import { Code, InlineCode } from '../components/Code';
+
+const components = {
+  inlineCode: props => <InlineCode {...props} />,
+  code: props => <Code {...props} />,
+  a: props => (
+    <a
+      {...props}
+      css={css`
+        color: inherit;
+        text-decoration: none;
+        border-bottom: 1px solid;
+      `}
+    />
+  ),
+};
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <MDXProvider components={components}>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </Container>
+    );
+  }
+}
