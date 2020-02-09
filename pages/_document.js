ee29686834eab031310c23dc992e82f7bcc67bef
Thibaut Dutartre <t.dutartre@gmail.com>
2020-02-09 12:41:30 +0100

    🇪🇺 Reduce to latin characters

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,24 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <link
+            rel="preload"
+            href="/static/cartograph/CartographCF-Regular-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossorigin="anonymous"
+          />
+          <Global
+            styles={css`
+              @font-face {
+                font-family: Cartograph;
+                src: url('/static/cartograph/CartographCF-Regular-latin.woff2') format('woff2'),
+                  url('/static/cartograph/CartographCF-Regular-latin.woff') format('woff');
+                font-weight: 400;
+                font-display: swap;
+              }
+            `}
+          />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144579302-1"></script>
           <script
@@ -18,24 +36,6 @@ export default class MyDocument extends Document {
               gtag('config', 'UA-144579302-1');
           `,
             }}
-          />
-          <link
-            rel="preload"
-            href="/static/cartograph/CartographCF-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossorigin
-          />
-          <Global
-            styles={css`
-              @font-face {
-                font-family: Cartograph;
-                src: url('/static/cartograph/CartographCF-Regular.woff2') format('woff2'),
-                  url('/static/cartograph/CartographCF-Regular.woff') format('woff');
-                font-weight: 400;
-                font-display: swap;
-              }
-            `}
           />
         </Head>
         <body>
