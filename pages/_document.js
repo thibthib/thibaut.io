733cc1202a9333d0b39d868374033e64c26edaaa
Thibaut Dutartre <t.dutartre@gmail.com>
2020-05-03 18:29:14 +0200

    🐶 Better DD loading

--- pages/_document.js
+++ pages/_document.js
@@ -59,18 +59,6 @@ export default class MyDocument extends Document {
           />
           <link rel="icon" href="favicon.svg" type="image/svg+xml" />
           <link rel="alternate icon" href="/favicon.png" type="image/png" />
-          <script src="https://www.datadoghq-browser-agent.com/datadog-rum-us.js"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.DD_RUM &&
-              location.hostname !== 'localhost' &&
-              window.DD_RUM.init({
-                clientToken: 'puba9fe9045f841f0404364249bbd53f08e',
-                applicationId: '9a14efdd-aeb9-4f66-ba0a-232b42032043',
-              });`,
-            }}
-          />
         </Head>
         <body>
           <Main />
