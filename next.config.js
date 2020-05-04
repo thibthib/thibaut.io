0ef7415966d889a50770952acb7f0f7fa2451677
Thibaut Dutartre <t.dutartre@gmail.com>
2020-05-04 16:29:12 +0200

    👀 Add analyzer

--- next.config.js
+++ next.config.js
@@ -1,20 +1,34 @@
 const withMDX = require('@next/mdx')();
 const withTM = require('next-transpile-modules')(['react-syntax-highlighter']);
 
-module.exports = withTM(
-  withMDX({
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    experimental: {
-      redirects() {
-        return [
-          {
-            source: '/refs',
-            destination: '/react-refs-evolution',
-            permanent: true,
-          },
-        ];
+const analyzing = process.env.ANALYZE === 'true';
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: analyzing,
+});
+
+module.exports = withBundleAnalyzer(
+  withTM(
+    withMDX({
+      reactStrictMode: true,
+      pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+      experimental: {
+        redirects() {
+          return [
+            {
+              source: '/refs',
+              destination: '/react-refs-evolution',
+              permanent: true,
+            },
+          ];
+        },
       },
-    },
-  })
+      webpack: config => {
+        config.optimization.concatenateModules = analyzing
+          ? false
+          : config.optimization.concatenateModules;
+        return config;
+      },
+    })
+  )
 );
