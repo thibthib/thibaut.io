66a0315e06be4fb803eb0c123daa6a0db108f1b6
Thibaut Dutartre <t.dutartre@gmail.com>
2020-06-04 15:53:04 +0200

    🖼 Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { datadogRum } from '@datadog/browser-rum';
+import { Global, css } from '@emotion/core';
 import { MDXProvider } from '@mdx-js/react';
 import { Emphasis } from 'components/Emphasis';
 import { Code, InlineCode, Pre } from 'components/Code';
@@ -34,6 +36,43 @@ const components = {
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider>
+      <Head>
+        <link
+          rel="preload"
+          href="archia-regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="true"
+        />
+        <link rel="icon" href="favicon.svg" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <meta name="theme-color" content="#171623" />
+      </Head>
+      <Global
+        styles={css`
+          @font-face {
+            font-family: Cartograph;
+            src: url('cartograph-regular.woff2') format('woff2'),
+              url('cartograph-regular.woff') format('woff');
+            font-display: swap;
+          }
+
+          @font-face {
+            font-family: Cartograph;
+            src: url('cartograph-regular-italic.woff2') format('woff2'),
+              url('cartograph-regular-italic.woff') format('woff');
+            font-style: italic;
+            font-display: swap;
+          }
+
+          @font-face {
+            font-family: Archia;
+            src: url('archia-regular.woff2') format('woff2'),
+              url('archia-regular.woff') format('woff');
+            font-display: swap;
+          }
+        `}
+      />
       <MDXProvider components={components}>
         <Component {...pageProps} />
       </MDXProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,51 +1,17 @@
-import Document, { Main, NextScript, Head } from 'next/document';
-import { Global, css } from '@emotion/core';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
-        <Head>
-          <link
-            rel="preload"
-            href="archia-regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="true"
-          />
-          <Global
-            styles={css`
-              @font-face {
-                font-family: Cartograph;
-                src: url('cartograph-regular.woff2') format('woff2'),
-                  url('cartograph-regular.woff') format('woff');
-                font-display: swap;
-              }
-
-              @font-face {
-                font-family: Cartograph;
-                src: url('cartograph-regular-italic.woff2') format('woff2'),
-                  url('cartograph-regular-italic.woff') format('woff');
-                font-style: italic;
-                font-display: swap;
-              }
-
-              @font-face {
-                font-family: Archia;
-                src: url('archia-regular.woff2') format('woff2'),
-                  url('archia-regular.woff') format('woff');
-                font-display: swap;
-              }
-            `}
-          />
-          <link rel="icon" href="favicon.svg" type="image/svg+xml" />
-          <link rel="alternate icon" href="/favicon.png" type="image/png" />
-        </Head>
+      <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
+
+export default MyDocument;
