27e3e5dcfd5aae51fad0d846bce1cb5b33310a98
Thibaut Dutartre <thibaut.dutartre@datadoghq.com>
2021-01-18 14:55:44 +0100

    Trying webpack plugin

--- next.config.js
+++ next.config.js
@@ -1,40 +1,57 @@
 const mdxPrism = require("mdx-prism");
-const withTreat = require("next-treat")();
 const withMDX = require("@next/mdx")({
   options: {
     rehypePlugins: [mdxPrism],
   },
 });
 
+const {
+  getClientStyleLoader,
+} = require("next/dist/build/webpack/config/blocks/css/loaders/client");
+const TreatPlugin = require("treat/webpack-plugin");
+
 const analyzing = process.env.ANALYZE === "true";
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: analyzing,
 });
 
-module.exports = withTreat(
-  withBundleAnalyzer(
-    withMDX({
-      reactStrictMode: true,
-      pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-      redirects: async function () {
-        return [
-          {
-            source: "/refs",
-            destination: "/react-refs-evolution",
-            permanent: true,
-          },
-        ];
-      },
-      webpack: (config) => {
-        config.optimization.concatenateModules = analyzing
-          ? false
-          : config.optimization.concatenateModules;
-        config.module.rules.push({
-          test: /react-spring/,
-          sideEffects: true,
-        });
-        return config;
-      },
-    })
-  )
+module.exports = withBundleAnalyzer(
+  withMDX({
+    reactStrictMode: true,
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    redirects: async function () {
+      return [
+        {
+          source: "/refs",
+          destination: "/react-refs-evolution",
+          permanent: true,
+        },
+      ];
+    },
+    webpack: (config, options) => {
+      config.optimization.concatenateModules = analyzing
+        ? false
+        : config.optimization.concatenateModules;
+      config.module.rules.push({
+        test: /react-spring/,
+        sideEffects: true,
+      });
+      config.plugins.push(
+        new TreatPlugin({
+          outputCSS: !options.isServer,
+          ...(options.isServer
+            ? {}
+            : {
+                outputLoaders: [
+                  getClientStyleLoader({
+                    isDevelopment: options.dev,
+                    assetPrefix: options.config.assetPrefix,
+                  }),
+                ],
+              }),
+        })
+      );
+      return config;
+    },
+  })
 );
