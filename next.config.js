0a8b0c5dfb1291330cfa7e6e6c0681e35c461f75
Thibaut Dutartre <thibaut.dutartre@datadoghq.com>
2021-03-12 19:42:55 +0100

    ✨ Better theme page

--- next.config.js
+++ next.config.js
@@ -4,47 +4,50 @@ const withMDX = require("@next/mdx")({
     rehypePlugins: [mdxPrism],
   },
 });
+const withTM = require("next-transpile-modules")(["@color-spaces/convert"]);
 
 const analyzing = process.env.ANALYZE === "true";
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: analyzing,
 });
 
-module.exports = withBundleAnalyzer(
-  withMDX({
-    reactStrictMode: true,
-    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-    redirects: async function () {
-      return [
-        {
-          source: "/refs",
-          destination: "/react-refs-evolution",
-          permanent: true,
-        },
-      ];
-    },
-    async headers() {
-      return [
-        {
-          source: "/fonts/:font*",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, immutable, max-age=31536000",
-            },
-          ],
-        },
-      ];
-    },
-    webpack: (config) => {
-      config.optimization.concatenateModules = analyzing
-        ? false
-        : config.optimization.concatenateModules;
-      config.module.rules.push({
-        test: /react-spring/,
-        sideEffects: true,
-      });
-      return config;
-    },
-  })
+module.exports = withTM(
+  withBundleAnalyzer(
+    withMDX({
+      reactStrictMode: true,
+      pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+      redirects: async function () {
+        return [
+          {
+            source: "/refs",
+            destination: "/react-refs-evolution",
+            permanent: true,
+          },
+        ];
+      },
+      async headers() {
+        return [
+          {
+            source: "/fonts/:font*",
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "public, immutable, max-age=31536000",
+              },
+            ],
+          },
+        ];
+      },
+      webpack: (config) => {
+        config.optimization.concatenateModules = analyzing
+          ? false
+          : config.optimization.concatenateModules;
+        config.module.rules.push({
+          test: /react-spring/,
+          sideEffects: true,
+        });
+        return config;
+      },
+    })
+  )
 );
